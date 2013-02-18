047e998aa8b1aa0c4b19ec33b7485b931f9d8e74
jeremymills <jeremy.mills89@gmail.com>
2013-02-18 18:39:10 -0500

    initialization of singly and doubly node classes

--- src/Data/LinkedLists/DoublyLinkedList.php
+++ src/Data/LinkedLists/DoublyLinkedList.php
@@ -5,9 +5,21 @@
  * @ignore
  */
 namespace Data\LinkedLists;
+/**
+ * Require INode
+ * @ignore
+ */
+require_once __DIR__ . '/Node.php';
+/**
+ * POD Class to hold Plain Old Data
+ *
+ * @package POD
+ * @author Jeremy Mills <j_mills44@fanshaweonline.ca>
+ * @copyright (c) Jeremy Mills
+ * @version PHP 5.3.10
+ */
 
-
-
-class DoublyLinkedList implements IDoublyLinkedList {
+class DoublyLinkedList implements IDoublyLinkedList
+{
     
 }
